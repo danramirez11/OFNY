5aca8f0979523f48aeb74956077fbc95d6ac6d5d
Dan <daramirezp1112@gmail.com>
2023-09-18 10:28:03 -0500

    style posts main

--- src/data/postData.ts
+++ src/data/postData.ts
@@ -1,32 +1,150 @@
 export const postData = [
     {
-        username: "solangelo",
-        profilepicture: "https://i.pinimg.com/564x/60/a3/43/60a343fe74840522c58f4381c485d70b.jpg",
-        post: "https://i.pinimg.com/564x/2d/57/fe/2d57fe2d7e6d07f38b92c570d341f15a.jpg"
+        username: "saggu",
+        profilepicture: "https://i.pinimg.com/564x/e0/fb/2d/e0fb2da579d0de649bd1285b4ae24abf.jpg",
+        post: "https://i.pinimg.com/564x/43/9e/f9/439ef96f2f9811d0573e78483c0627f7.jpg"
     },
     {
-        username: "solangelo",
-        profilepicture: "https://i.pinimg.com/564x/60/a3/43/60a343fe74840522c58f4381c485d70b.jpg",
-        post: "https://i.pinimg.com/564x/2d/57/fe/2d57fe2d7e6d07f38b92c570d341f15a.jpg"
+        username: "tynny56",
+        profilepicture: "https://i.pinimg.com/564x/12/a4/3f/12a43f5a54e6feddbaff037db21a2627.jpg",
+        post: "https://i.pinimg.com/564x/05/4e/60/054e6004b69de932cedb13ef686287ee.jpg"
     },
     {
-        username: "solangelo",
-        profilepicture: "https://i.pinimg.com/564x/60/a3/43/60a343fe74840522c58f4381c485d70b.jpg",
-        post: "https://i.pinimg.com/564x/2d/57/fe/2d57fe2d7e6d07f38b92c570d341f15a.jpg"
+        username: "rosseeee",
+        profilepicture: "https://i.pinimg.com/564x/14/63/08/146308762ff1efa7eb677dfff839df4a.jpg",
+        post: "https://i.pinimg.com/564x/90/70/82/907082fdaabdb49222db44d6a49b4b30.jpg"
     },
     {
-        username: "solangelo",
-        profilepicture: "https://i.pinimg.com/564x/60/a3/43/60a343fe74840522c58f4381c485d70b.jpg",
-        post: "https://i.pinimg.com/564x/2d/57/fe/2d57fe2d7e6d07f38b92c570d341f15a.jpg"
+        username: "minniee",
+        profilepicture: "https://i.pinimg.com/564x/de/60/1f/de601f041e30056af0ef01ddde10abb3.jpg",
+        post: "https://i.pinimg.com/564x/37/ef/b0/37efb0c02a8b230fe0d027425d8764bc.jpg"
     },
     {
-        username: "solangelo",
-        profilepicture: "https://i.pinimg.com/564x/60/a3/43/60a343fe74840522c58f4381c485d70b.jpg",
-        post: "https://i.pinimg.com/564x/2d/57/fe/2d57fe2d7e6d07f38b92c570d341f15a.jpg"
+        username: "ilashalimov",
+        profilepicture: "https://i.pinimg.com/564x/94/9f/34/949f347d2afeae7df7087b458cd86149.jpg",
+        post: "https://i.pinimg.com/564x/74/86/a8/7486a86ce9bb88bc8e5968072403302f.jpg"
     },
     {
-        username: "solangelo",
-        profilepicture: "https://i.pinimg.com/564x/60/a3/43/60a343fe74840522c58f4381c485d70b.jpg",
-        post: "https://i.pinimg.com/564x/2d/57/fe/2d57fe2d7e6d07f38b92c570d341f15a.jpg"
+        username: "y u m ",
+        profilepicture: "https://i.pinimg.com/564x/fc/67/f3/fc67f3d1d28d63d57373b79532fe03c8.jpg",
+        post: "https://i.pinimg.com/564x/ec/0f/a1/ec0fa1b2aa54f66ad784f4089d5bc9a6.jpg"
     },
+    {
+        username: "livliv",
+        profilepicture: "https://i.pinimg.com/564x/00/03/26/0003265494cd86c14da12d1dce4ee459.jpg",
+        post: "https://i.pinimg.com/564x/69/2f/6c/692f6c12bf7b87da89cf08a51963d1e9.jpg"
+    },
+    {
+        username: "gabby",
+        profilepicture: "https://i.pinimg.com/564x/6a/d6/00/6ad6003f544fc382d25bd41605d89281.jpg",
+        post: "https://i.pinimg.com/564x/88/f9/84/88f984509dcbc219b03b64ea159e3553.jpg"
+    },
+    {
+        username: "wonnie",
+        profilepicture: "https://i.pinimg.com/564x/9f/bd/dd/9fbddd722624e74a5630566878b29926.jpg",
+        post: "https://i.pinimg.com/564x/4d/19/71/4d1971ab16e41f146231f4fcf10ed15b.jpg"
+    },
+    {
+        username: "Hyunnie",
+        profilepicture: "https://i.pinimg.com/564x/ce/01/5d/ce015df517f9b8eec6872ea646ab64b9.jpg",
+        post: "https://i.pinimg.com/564x/a6/5b/ec/a65bec255a10bb4eb2b6bf6ac991ad5c.jpg"
+    },
+    {
+        username: "Elann",
+        profilepicture: "https://i.pinimg.com/564x/e2/e3/bb/e2e3bbc4844f07f410cb2dc197e3bfcc.jpg",
+        post: "https://i.pinimg.com/564x/54/0b/fb/540bfb61d11ad655b2faeb7710473886.jpg"
+    },
+    {
+        username: "Appelpi",
+        profilepicture: "https://i.pinimg.com/564x/dd/f3/e1/ddf3e1b9d5a3c98e0359a46ed86a1cbd.jpg",
+        post: "https://i.pinimg.com/564x/98/63/f8/9863f8c645eb0c3ad4f4c684fec43787.jpg"
+    },
+    {
+        username: "AudreyLoop",
+        profilepicture: "https://i.pinimg.com/564x/d6/84/2a/d6842ab2fd9b9560086681164c34adcf.jpg",
+        post: "https://i.pinimg.com/564x/f1/19/f7/f119f7017037490485f45eba01123d75.jpg"
+    },
+    {
+        username: "SOIR",
+        profilepicture: "https://i.pinimg.com/564x/5d/a2/e0/5da2e0cf22b17984f84a23af49b450d4.jpg",
+        post: "https://i.pinimg.com/564x/6b/08/1a/6b081a16e20ceb77d994429716b1a8c9.jpg"
+    },
+    {
+        username: "chanBang",
+        profilepicture: "https://i.pinimg.com/564x/21/b6/a7/21b6a7f36bf8fd70530ce93b549412f4.jpg",
+        post: "https://i.pinimg.com/564x/cc/48/4c/cc484cae94ab4d47d9df3430bf0f9812.jpg"
+    },
+    {
+        username: "miaVal",
+        profilepicture: "https://i.pinimg.com/564x/79/38/a8/7938a8231b9095bae471c5807e575a12.jpg",
+        post: "https://i.pinimg.com/564x/e2/ae/5a/e2ae5aa79827d422d6a21f3e16636b63.jpg"
+    },
+    {
+        username: "jenKhugs",
+        profilepicture: "https://i.pinimg.com/564x/f3/4c/5d/f34c5d14c1873c988c10d37d7f9f8724.jpg",
+        post: "https://i.pinimg.com/564x/a8/59/9c/a8599cdab3779d9725286bdf9dd0596d.jpg"
+    },
+    {
+        username: "Bin97",
+        profilepicture: "https://i.pinimg.com/564x/bd/72/c6/bd72c6e1d6d2e4b5355d54a14cf9b727.jpg",
+        post: "https://i.pinimg.com/564x/e4/38/23/e4382314ef40875d1a02461739849df5.jpg"
+    },
+    {
+        username: "Rinne",
+        profilepicture: "https://i.pinimg.com/750x/4f/e4/67/4fe467cb78fe00b1760bead0cbcd47c1.jpg",
+        post: "https://i.pinimg.com/564x/ba/9b/71/ba9b716bb1c3548adbe4c5d807f4a3c8.jpg"
+    },
+    {
+        username: "Baldeeo",
+        profilepicture: "https://i.pinimg.com/564x/ce/3d/1c/ce3d1ca2294abb5a92064dd37dcacb25.jpg",
+        post: "https://i.pinimg.com/564x/44/d6/be/44d6bebc00178dd7f407cec6861834bb.jpg"
+    },
+    {
+        username: "outfitswithkay",
+        profilepicture: "https://i.pinimg.com/564x/82/63/2f/82632fb4a7571401ca8b03bac59aff54.jpg",
+        post: "https://i.pinimg.com/564x/34/dc/0a/34dc0af749b444d54d490434f4deec98.jpg"
+    },
+    {
+        username: "Leemin",
+        profilepicture: "https://i.pinimg.com/564x/23/d9/4a/23d94a763406c434a76dac3683a587fc.jpg",
+        post: "https://i.pinimg.com/564x/ae/97/3a/ae973a47f34a88dc31c6fc4db2488bad.jpg"
+    },
+    {
+        username: "lixxieee",
+        profilepicture: "https://i.pinimg.com/564x/92/b2/df/92b2df3f4c5c87fa7f21eb3973f09ca0.jpg",
+        post: "https://i.pinimg.com/564x/49/94/de/4994de9681d682786901a76321ab7571.jpg"
+    },
+    {
+        username: "jaeenan",
+        profilepicture: "https://i.pinimg.com/564x/71/70/fa/7170fac358a41d05fd92290935c575ed.jpg",
+        post: "https://i.pinimg.com/564x/db/58/9b/db589bd912bd49de9381a8c9696ab35c.jpg"
+    },
+    {
+        username: "Hyen",
+        profilepicture: "https://i.pinimg.com/564x/cf/1a/0a/cf1a0ab90705cc122633003dad2f4164.jpg",
+        post: "https://i.pinimg.com/564x/e7/77/03/e777030ab9d6f4bbeaa57338f1192c11.jpg"
+    },
+    {
+        username: "HanJisung",
+        profilepicture: "https://i.pinimg.com/564x/bd/86/36/bd86361152552d1703f544ec20536dd9.jpg",
+        post: "https://i.pinimg.com/564x/df/f9/90/dff99029490a191aa74be49926d562a9.jpg"
+    },
+    {
+        username: "winterInclothes",
+        profilepicture: "https://i.pinimg.com/564x/d5/31/36/d53136640c9993f98ce9d23da6430783.jpg",
+        post: "https://i.pinimg.com/564x/35/bd/19/35bd190710066982e77389e7fa5e67c3.jpg"
+    },
+    {
+        username: "Plutoo",
+        profilepicture: "https://i.pinimg.com/564x/fb/67/32/fb67322707c21c4b51543ab196a738e2.jpg",
+        post: "https://i.pinimg.com/564x/91/3a/4f/913a4fdcfe0cf2f669bb9e314c7dd9c5.jpg"
+    },
+
+
+
+
+
+
+
+
 ]
