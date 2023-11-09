b66e20fe6b3f81b8bdceb185953d3de3cd32e996
Dan <daramirezp1112@gmail.com>
2023-11-09 12:59:04 -0500

    fix firebase folders

---
+++ src/utils/firebase.ts
@@ -0,0 +1,88 @@
+import { firebaseConfig } from "./firebaseconfig";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, setPersistence, browserLocalPersistence} from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export const addPost = async (post: any) => {
+  try{
+      const where = collection(db, "posts")
+      await addDoc(where, post)
+  } catch (error) {
+      console.error(error)
+  }
+}
+export const getPost = async () => {
+  const querySnapshot = await getDocs(collection(db, "posts"));
+  const transformed: any = [];
+
+  querySnapshot.forEach((doc: any) => {
+      const data = doc.data();
+      transformed.push({id: doc.id, ...data})
+  });
+
+  return transformed;
+}
+
+export const getProfile = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  const transformed: any = [];
+
+  querySnapshot.forEach((doc: any) => {
+      const data = doc.data();
+      transformed.push({id: doc.id, ...data})
+  });
+
+  return transformed;
+}
+
+/*export const createUser = (email: any, pass: any /other stuff lije username/) => {
+  createUserWithEmailAndPassword(auth, email, pass)
+  .then(async (userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user)
+    try{
+      const where = collection(db, "users", user.uid)
+      const data = {
+        /*name: name,
+        age: age,
+      }
+      await addUser(where, data)
+  } catch (error) {
+      console.error(error)
+  }
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+
+export const addUser = (a: any, b: any) => {
+
+}
+
+const logIn = (email:any, pass: any) => {
+  signInWithEmailAndPassword(auth, email, pass)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}*/
+
+
+
+export default {
+  addPost, getPost, getProfile
+}
