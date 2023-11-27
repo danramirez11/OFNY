5f4f1e100ee97656206616edbbe550ceb2ba69ce
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2023-11-27 18:47:07 -0500

    screen details post ts

--- src/screens/Details.ts
+++ src/screens/Details.ts
@@ -2,3 +2,30 @@ import * as components from "../components/export"
 import MainBar, { Attribute } from "../components/MainBar/MainBar";
 import BoxDetails from "../components//BoxDetails/BoxDetails";
 import DetailsStyle from "./Details.css";
+
+class DetailsPostContainer extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+            <style>${DetailsStyle}</style>
+            `;
+            const body = document.createElement("section")
+            this.shadowRoot.appendChild(body);
+            body.classList.add("body")
+
+            const BoxDetails = document.createElement("box-details");
+            body.appendChild(BoxDetails);
+        }
+    }
+}
+
+customElements.define("app-details", DetailsPostContainer);
