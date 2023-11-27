494d4473e2029af634fd5075c9e40fc47ac8a9fd
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2023-11-27 18:44:17 -0500

    import components to screen details

---
+++ src/screens/Details.ts
@@ -0,0 +1,4 @@
+import * as components from "../components/export"
+import MainBar, { Attribute } from "../components/MainBar/MainBar";
+import BoxDetails from "../components//BoxDetails/BoxDetails";
+import DetailsStyle from "./Details.css";
