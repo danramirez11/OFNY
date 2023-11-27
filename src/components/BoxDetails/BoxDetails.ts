0726f6c8380d454ddb5587e6ead79273acf50d2d
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2023-11-27 17:59:35 -0500

    componente y css

--- src/components/BoxDetails/BoxDetails.ts
+++ src/components/BoxDetails/BoxDetails.ts
@@ -65,28 +65,17 @@ class BoxDetails extends HTMLElement {
             this.shadowRoot.innerHTML = `
             <style>${BoxDetailsStyle}</style>
             <section>
-            <img id="image post" src="<>${this.imagepost}">
+            <img id="imagepost" src="<>${this.imagepost}">
             
-            <div class=post details>
-            <h1>${this.username}</h1>
-                <div class="input-container">
-                    <img src="https://cdn.discordapp.com/attachments/1108887572618412231/1168666493194616852/Group_326.png?ex=6552987c&is=6540237c&hm=30a4aadfdb9f701d4a16a30690fea37279108f9b9a370b504d814d708a614fd3&" alt="User Icon">
-                    <input placeholder="Username" type="text" id="username" name="username">
+            <div class="post details">
+                <div class="userdetails">
+                <img src="${this.profilepicture}" alt="User Profile Picture">
+                <h1>${this.username}</h1>
                 </div>
-                <div class="input-container">
-                    <img src="https://cdn.discordapp.com/attachments/1108887572618412231/1168666492703875122/Group_327.png?ex=6552987c&is=6540237c&hm=94a595cb017903b639e2a5e645e1f46817c2b8ea712cb87c6eec469073e0e245&" alt="Password Icon">
-                    <input placeholder="Password" type="password" id="password" name="password">
-                </div>
-                <div class="options">
-                    <img class="post-heart-desktop check" src="https://cdn.discordapp.com/attachments/1108887572618412231/1169285218528673862/Group_3111.png?ex=6554d8b8&is=654263b8&hm=59a1ef10175f86c38a8f7dda15798a76cf0bdf2f1ecc3cf9e6585f5f133c4f72&">
-                    <button id="Remember">Remember</button>
-                    <button id="Forgot-password">Forgot-password?</button>
-                </div>
-                <div class"button-login">
-                    <button id="login-button">Log In</button>  
-                </div>
-                <div class="options">
-                    <button id="no-acount">Don’t have an account? Sing Up</button>
+
+                <div class= "posterdetails">
+                <p id="caption" >${this.caption}</p>
+                <p id="tags" >${this.tags}</p>
                 </div>
             </div>
             </section>
