5bf7cf9056c20c704b2933400c03007fae49f6e8
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2023-11-30 18:54:50 -0500

    images firebase

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,32 @@ import firebase, { getProfile } from '../utils/firebase';
 import { Screens } from '../types/navigation';
 
 const user = await getProfile("z9R9t4beoGh2kwrwHirxaCDMO0r2")
+const images = {
+	pfp: 'https://firebasestorage.googleapis.com/v0/b/dcalg-7b097.appspot.com/o/media%2Fprofilepicture.png?alt=media&token=ff51078d-73f2-4bdc-9369-0a9229d65db4',
+	logo: "https://firebasestorage.googleapis.com/v0/b/dcalg-7b097.appspot.com/o/media%2Flogo.png?alt=media&token=2a208276-2249-4196-ba20-eaebd0711f46",
+	cormor: "https://firebasestorage.googleapis.com/v0/b/dcalg-7b097.appspot.com/o/media%2Fcorazon%20morado.png?alt=media&token=a4aba781-5f70-4451-923a-1953ce5d6225",
+	corblanc: "https://firebasestorage.googleapis.com/v0/b/dcalg-7b097.appspot.com/o/media%2Fcorazon%20blanco.png?alt=media&token=a810797c-fbf6-42c8-a920-555df8a45258",
+	corborde: "",
+	alien: "",
+	landing: "",
+	login: "",
+	backlogin: "",
+	user: "",
+	password: "",
+	check: "",
+	checkvacio: "",
+	subirpost: "",
+	ovniblanc: "",
+	ovnimor: "",
+	singup: "",
 
+}
 export let appState: AppState = {
 	screen: Screens.PROFILE,
 	editprofile: false,
 	user: user,
 	postid: " ",
+	images: images,
 };
 
 console.log(appState);
