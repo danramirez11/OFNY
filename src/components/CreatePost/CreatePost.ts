5cf19530ca64baf250498a85ef568ed0532200e9
Dan <daramirezp1112@gmail.com>
2023-12-01 00:22:31 -0500

    update tag options

--- src/components/CreatePost/CreatePost.ts
+++ src/components/CreatePost/CreatePost.ts
@@ -181,6 +181,7 @@ class CreatePost extends HTMLElement{
                         <option value="formal">Formal</option>
                         <option value="deportivo">Deportivo</option>
                         <option value="gótico">Gótico</option>
+                        <option value="cotaggecore">Cottagecore</option>
                     </select>
                     <section class="post-info-tags">
                     </section>
