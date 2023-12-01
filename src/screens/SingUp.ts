175578882b60a7dfe2a1effc4ade184b3de25533
Dan <daramirezp1112@gmail.com>
2023-12-01 12:52:44 -0500

    storage change screen auto sing up

--- src/screens/SingUp.ts
+++ src/screens/SingUp.ts
@@ -12,6 +12,9 @@ class SingUpContainer extends HTMLElement {
     }
 
     connectedCallback() {
+        if (appState.user.uid !== " "){
+            dispatch(navigate(Screens.DASHBOARD))
+        }
         this.render();
     }
 
